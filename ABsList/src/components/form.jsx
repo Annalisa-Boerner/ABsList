a9707d74245cb9235d4be0a77c231b777c1cddc3
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-08-19 09:47:28 -0400

    built out the form some more and added likely functioning checkbox; submit is still empty

--- ABsList/src/components/form.jsx
+++ ABsList/src/components/form.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { createPost } from "../../services/88index";
+import { useState } from "react";
 
 export default function NewPost({ posts, setPosts }) {
      const [title, setTitle] = useState("");
@@ -8,32 +7,73 @@ export default function NewPost({ posts, setPosts }) {
      const [description, setDescription] = useState("");
      const [willDeliver, setWilldeliver] = useState(false);
      const [error, setError] = useState(null);
+     const onChange = () => setWilldeliver(!willDeliver);
 
+     //needs all the API call language here
      return (
-          <form>
-               {error && (
-                    <p>
-                         We are sorry, there has been an error with your
-                         submission. Please try again l8r.
-                    </p>
-               )}
-               <input
-                    type="text"
-                    value={title}
-                    name="title"
-                    placeholder="title"
-                    onChange={(event) => setTitle(event.target.value)}
-               />
-               <input
-                    type="text"
-                    value={description}
-                    name="description"
-                    placeholder="description"
-                    onChange={(event) => setDescription(event.target.value)}
-               />
-               <button>Submit</button>
-          </form>
+          <section>
+               <h2>New Item Submission Form:</h2>
+               <form className="formStyles">
+                    {error && (
+                         <p>
+                              We are sorry, there has been an error with your
+                              submission. Please try again l8r.
+                         </p>
+                    )}
+                    <label>Title </label>
+                    <input
+                         type="text"
+                         value={title}
+                         name="Title"
+                         placeholder="title"
+                         onChange={(event) => setTitle(event.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <label>Description </label>
+                    <input
+                         type="text"
+                         value={description}
+                         name="Description"
+                         placeholder="description"
+                         onChange={(event) =>
+                              setDescription(event.target.value)
+                         }
+                    />
+                    <br />
+                    <br />
+                    <label>Price </label>
+                    <input
+                         type="text"
+                         value={price}
+                         name="Price"
+                         placeholder="Price"
+                         onChange={(event) => setPrice(event.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <label>Location </label>
+                    <input
+                         type="text"
+                         value={location}
+                         name="Location"
+                         placeholder="Location"
+                         onChange={(event) => setLocation(event.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <input
+                         type="checkbox"
+                         value={willDeliver}
+                         name="willDeliver"
+                         checked={willDeliver}
+                         onChange={onChange}
+                    />
+                    <label> Will Deliver</label>
+                    <br />
+                    <br />
+                    <button>Submit</button>
+               </form>
+          </section>
      );
-
-     // <h2>I am a form</h2>;
 }

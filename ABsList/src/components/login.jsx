b3ad2e916e373ca1b40b553e1b18f054f0d51563
root <root@AB-PC>
2023-08-20 12:04:44 -0400

    built login form; no API interaction, yet

--- ABsList/src/components/login.jsx
+++ ABsList/src/components/login.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react"
+
 export default function Login() {
-     return <h2>someday I shall be a login page</h2>;
+     const [username, setUsername] = useState('')
+     const [password, setPassword] = useState('')
+
+     async function handleSubmit() {
+          console.log('motherfucking button time')
+     }
+
+
+
+
+
+
+     return (<section>
+          <h2>Log In, Friend-o</h2>
+          {/* {error && <h2>{error}</h2>} */}
+          <form onSubmit={handleSubmit}>
+               <label>
+                    Username: <input type="text" placeholder='whats ur name' value={username} onChange={(event) => setUsername(event.target.value)} />
+               </label><br /><br />
+               <label>Password: <input type="password"
+                    placeholder="passwrod bby"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)} />
+               </label><br /><br />
+               <button>Submit, if you dare</button>
+
+
+
+
+
+          </form>
+
+
+
+     </section>)
 }

97b73e3a75edecfd182b7754875f9241ad2a8b2b
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-08-21 13:27:13 -0400

    search bar works

--- ABsList/src/components/allPosts.jsx
+++ ABsList/src/components/allPosts.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import { fetchAllPosts } from "../../services/apiCalls";
-import SearchBar from "./searchBar";
 
 export default function AllPosts() {
      const [posts, setPosts] = useState([]);
+     const [searchParam, setSearchParam] = useState("");
 
      async function usefulPosts() {
           let postArray = await fetchAllPosts();
@@ -13,16 +13,36 @@ export default function AllPosts() {
           usefulPosts();
      }, []);
 
+     const postsToDisplay = searchParam
+          ? posts.filter((post) =>
+                 post.title.toLowerCase().includes(searchParam)
+            )
+          : posts;
+
      return (
           <div>
-               <SearchBar />
                <h1>Welcome to AB List, your one stop used item shop!</h1>
-               {posts.map((post) => {
+               <div>
+                    <label>
+                         Search:{""}
+                         <input
+                              type="text"
+                              placeholder="search"
+                              onChange={(event) =>
+                                   setSearchParam(
+                                        event.target.value.toLowerCase()
+                                   )
+                              }
+                         />
+                    </label>
+               </div>
+               {postsToDisplay.map((post) => {
                     return (
                          <div key={post._id}>
-                              <h2>{post.title}</h2>
-                              <p>{post.description}</p>
-                              <p>{post.price}</p>
+                              <h2>Title: {post.title}</h2>
+                              <p>Description: {post.description}</p>
+                              <p>Price: {post.price}</p>
+                              <p>Location: {post.location}</p>
                          </div>
                     );
                })}

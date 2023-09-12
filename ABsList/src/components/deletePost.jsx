20681ae6179004c6a566cb70ab0b801ad9c02860
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-09-12 17:52:52 -0400

    delete works, but not after an edit, only freshly after a login. maybe useNavigate(0) isn't a think

--- ABsList/src/components/deletePost.jsx
+++ ABsList/src/components/deletePost.jsx
@@ -2,15 +2,15 @@ import { deletePost } from "../../services/apiCalls";
 import { useNavigate } from "react-router-dom";
 
 export default function DeletePost({ post, token }) {
-     console.log("entering DeletePost component, then token", token);
-     console.log("this is the post ", post);
+     //  console.log("entering DeletePost component, then token", token);
+     //  console.log("this is the post ", post);
      const navigate = useNavigate();
 
      async function handleDelete(event) {
           event.preventDefault();
           try {
                const result = await deletePost(post._id, token);
-               console.log("line 13 token and post._id", token, post._id);
+               //    console.log("line 13 token and post._id", token, post._id);
                //    navigate(0);
           } catch (error) {
                console.error(error);
